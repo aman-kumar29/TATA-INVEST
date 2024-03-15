0656557bd5d32f1d4584b236408128c5c4c3c2ec
Aditya Lahane <adityalahane31@gmail.com>
2024-03-15 15:10:42 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1> Tata Invest </h1>
-        <p> Welcome to Tata Invest </p>
+        <p> Welcome to Tata Invest Wesite</p>
       </header>
     </div>
   );
