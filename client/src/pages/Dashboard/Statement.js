355e573c25a45fdd5fc2cc1a40bc1df48e58e84e
Aditya Lahane <adityalahane31@gmail.com>
2024-03-27 03:26:08 +0530

    Updated

--- client/src/pages/Dashboard/Statement.js
+++ client/src/pages/Dashboard/Statement.js
@@ -53,29 +53,32 @@ const Statement = () => {
 
 
     return (
-        <div className="container mt-5 my-5">
-            <h5 className="mt-2">Account Summary</h5>
-            <div className="account-details shadow profile-card mx-5">
-                <div className="summary-items">
-                    <div className="summary-item">
-                        <p>Invested Amount:</p><p> ₹ {investedAmount}</p>
-                    </div>
-                    <div className="summary-item">
-                        <p>Interest Income:</p><p> ₹ {userData?.interestAmount}</p>
-                    </div>
-                    <div className="summary-item">
-                        <p>Referral Income: </p><p>₹ {referralAmount}</p>
-                    </div>
-                    <div className="summary-item">
-                        <p>Life Time Earning: </p><p>₹ {referralAmount + userData?.interestAmount}</p>
-                    </div>
-                    <div className="summary-item">
-                        <p>Total Balance Withdrawable: </p><p>₹ {userData?.withdrawableAmount}</p>
-                    </div>
-                </div>
-            </div>
+        <div className="mx-2 mt-3 my-5">
+           <h1 className="text-center my-3">Account Summary</h1>
+           <div className="summary-box">
+    <div className="summary-item">
+        <p className="item-label">Invested Amount:</p>
+        <p className="item-value">₹ {investedAmount}</p>
+    </div>
+    <div className="summary-item">
+        <p className="item-label">Interest Income:</p>
+        <p className="item-value">₹ {userData?.interestAmount}</p>
+    </div>
+    <div className="summary-item">
+        <p className="item-label">Referral Income:</p>
+        <p className="item-value">₹ {referralAmount}</p>
+    </div>
+    <div className="summary-item">
+        <p className="item-label">Life Time Earning:</p>
+        <p className="item-value">₹ {referralAmount + userData?.interestAmount}</p>
+    </div>
+    <div className="summary-item">
+        <p className="item-label">Balance Withdrawable:</p>
+        <p className="item-value">₹ {userData?.withdrawableAmount}</p>
+    </div>
+</div>
 
-            <h1 className="mx-5 mt-5">Recent Transactions</h1>
+            <h1 className="mt-5 text-center my-3">Recent Transactions</h1>
             <div className="d-flex justify-content-center">
                 <button className={selectedOption === 'investments' ? 'btn btn-success mx-3' : 'btn btn-outline-success mx-3'} onClick={() => handleToggle('investments')}>
                     Investments
@@ -85,7 +88,7 @@ const Statement = () => {
                 </button>
             </div>
 
-            <div className="text-center mx-0 my-3">
+            <div className="text-center my-3 box">
                 <h1 className="mx-3">{selectedOption === 'investments' ? 'Investments' : 'Withdrawals'}</h1>
                 {userData ? (
                     selectedOption === 'investments' ? (
